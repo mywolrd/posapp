54f3b5f254928233116efe2697c75f6e192a5c8f
mywolrd <moonbae77@gmail.com>
2016-10-18 05:44:31 -0700

    Save ItemType

---
+++ src/main/java/com/pos/model/parameter/ItemTypeParameter.java
@@ -0,0 +1,34 @@
+package com.pos.model.parameter;
+
+public class ItemTypeParameter {
+	
+	private long id;
+	private String name;
+	private int weight;
+	private boolean active;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+}

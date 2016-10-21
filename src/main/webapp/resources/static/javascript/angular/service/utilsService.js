fd2ff5c8c484a08ae1543093531788102acefa6c
mywolrd <moonbae77@gmail.com>
2016-10-21 04:33:47 -0700

    Static CSS class strings

--- src/main/webapp/resources/static/javascript/angular/service/utilsService.js
+++ src/main/webapp/resources/static/javascript/angular/service/utilsService.js
@@ -1,16 +1,28 @@
 function utilsService() {
+	let BOOTSTRAP_CSS_COL_XS = "col-xs-",
+		TEXT_FONT_SIZE = 'font-';
 	
 	return {
 		getMaxPageNum: function(array, pageSize) {
 			if (angular.isDefined(array) && angular.isObject(array)) {
-				var len = array.length;
-				var div = Math.floor(len / pageSize);
-				var rem = len % pageSize;
+				let len = array.length,
+					div = Math.floor(len / pageSize),
+					rem = len % pageSize;
 				if (rem > 0) div++;
 				div--;
 				return div;
 			}
 			return 0;
+		},
+		
+		getCSScolxs: function() {
+			return BOOTSTRAP_CSS_COL_XS;
+		},
+		
+		getCSStextFontSize: function() {
+			return TEXT_FONT_SIZE;
 		}
+	
+	
 	};
 }

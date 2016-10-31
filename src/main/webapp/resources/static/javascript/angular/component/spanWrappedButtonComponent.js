722402cdcfaa531ff9d411fe34ca755ad4527500
mywolrd <moonbae77@gmail.com>
2016-10-30 23:49:52 -0700

    Check for undefined

--- src/main/webapp/resources/static/javascript/angular/component/spanWrappedButtonComponent.js
+++ src/main/webapp/resources/static/javascript/angular/component/spanWrappedButtonComponent.js
@@ -5,20 +5,26 @@ let spanWrappedButtonComponent = {
 			
 			ctrl.$onInit = function() {
 				ctrl.span_class = [];
-				ctrl.span_class.push(utilsService.getCSScolxs() + ctrl.spanWidth);
+				if (ctrl.spanWidth)
+					ctrl.span_class.push(utilsService.getCSScolxs() + ctrl.spanWidth);
 			
 				ctrl.button_class = [];
-				ctrl.button_class.push(ctrl.buttonClass);
+				if (ctrl.buttonClass)
+					ctrl.button_class.push(ctrl.buttonClass);
+				
+				if (! ctrl.type)
+					ctrl.type = 'button';
 			}
 		},
 	bindings: {
 		spanWidth:'@',
 		buttonName:'@',
 		buttonClass:'@',
+		type:'@',
 		doClick:'&'
 	},
 	template:
 			'<span class="no-padding" data-ng-class="$ctrl.span_class">'
-		+		'<button data-ng-class="$ctrl.button_class" class="btn btn-block form-control" data-ng-click="$ctrl.doClick()">{{$ctrl.buttonName}}</button>'
+		+		'<button type="$ctrl.type" data-ng-class="$ctrl.button_class" class="btn btn-block form-control" data-ng-click="$ctrl.doClick()">{{$ctrl.buttonName}}</button>'
 		+	'</span>'
 }

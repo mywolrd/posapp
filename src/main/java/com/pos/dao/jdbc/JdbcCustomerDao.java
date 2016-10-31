adba4df57405eb6bea19a03e4a0b72438cd51076
mywolrd <moonbae77@gmail.com>
2016-10-30 23:53:29 -0700

    Make last name search parameter upper case

--- src/main/java/com/pos/dao/jdbc/JdbcCustomerDao.java
+++ src/main/java/com/pos/dao/jdbc/JdbcCustomerDao.java
@@ -19,7 +19,7 @@ import com.pos.model.parameter.CustomerParameter;
 @Repository
 public class JdbcCustomerDao extends JdbcBaseDao implements CustomerDao {
 
-    private final static String listByLastName = "SELECT * from CUSTOMERS where CUSTOMERS.lastName like :lastName";
+    private final static String listByLastName = "SELECT * from CUSTOMERS where UPPER(CUSTOMERS.lastName) like :lastName";
     private final static String save = "INSERT into CUSTOMERS (lastName, firstName, pnumber, createdAt) values (:lastName, :firstName, :number, now()) ";
     private final static String update = "UPDATE CUSTOMERS set lastName = :lastName, firstName = :firstName, pnumber = :number, updatedAt = now() where customers.id = :id";
     private final static String uniqueById = "SELECT * from CUSTOMERS where CUSTOMERS.id = :id";
@@ -29,14 +29,17 @@ public class JdbcCustomerDao extends JdbcBaseDao implements CustomerDao {
 
     @Override
     public long save(CustomerParameter info) {
-        SqlParameterSource parameter = new MapSqlParameterSource().addValue(DBNames.LASTNAME, info.getLastName()).addValue(DBNames.FIRSTNAME, info.getFirstName())
+        SqlParameterSource parameter = new MapSqlParameterSource()
+                .addValue(DBNames.LASTNAME, info.getLastName())
+                .addValue(DBNames.FIRSTNAME, info.getFirstName())
                 .addValue(DBNames.NUMBER, info.getPhoneNumber());
         try {
             KeyHolder holder = new GeneratedKeyHolder();
             this.namedParameterJdbcTemplate.update(save, parameter, holder);
 
             if (!holder.getKeyList().isEmpty()) {
-                Integer id = (Integer) holder.getKeyList().get(0).get(DBNames.ID);
+                Integer id = (Integer) holder.getKeyList().get(0)
+                        .get(DBNames.ID);
                 return id;
             }
         } catch (DataAccessException e) {
@@ -47,14 +50,17 @@ public class JdbcCustomerDao extends JdbcBaseDao implements CustomerDao {
 
     @Override
     public long update(CustomerParameter info) {
-        SqlParameterSource parameter = new MapSqlParameterSource().addValue(DBNames.LASTNAME, info.getLastName()).addValue(DBNames.FIRSTNAME, info.getFirstName())
+        SqlParameterSource parameter = new MapSqlParameterSource()
+                .addValue(DBNames.LASTNAME, info.getLastName())
+                .addValue(DBNames.FIRSTNAME, info.getFirstName())
                 .addValue(DBNames.NUMBER, info.getPhoneNumber());
         try {
             KeyHolder holder = new GeneratedKeyHolder();
             this.namedParameterJdbcTemplate.update(update, parameter, holder);
 
             if (!holder.getKeyList().isEmpty()) {
-                Integer id = (Integer) holder.getKeyList().get(0).get(DBNames.ID);
+                Integer id = (Integer) holder.getKeyList().get(0)
+                        .get(DBNames.ID);
                 return id;
             }
         } catch (DataAccessException e) {
@@ -65,9 +71,11 @@ public class JdbcCustomerDao extends JdbcBaseDao implements CustomerDao {
 
     @Override
     public List<Customer> searchByLikeLastName(String querystr) {
-        SqlParameterSource parameter = new MapSqlParameterSource().addValue(DBNames.LASTNAME, querystr + "%");
+        SqlParameterSource parameter = new MapSqlParameterSource()
+                .addValue(DBNames.LASTNAME, querystr.toUpperCase() + "%");
         try {
-            return this.namedParameterJdbcTemplate.query(listByLastName, parameter, rowMapper);
+            return this.namedParameterJdbcTemplate.query(listByLastName,
+                    parameter, rowMapper);
         } catch (DataAccessException e) {
             return new LinkedList<Customer>();
         }
@@ -75,9 +83,11 @@ public class JdbcCustomerDao extends JdbcBaseDao implements CustomerDao {
 
     @Override
     public Customer uniqueById(long id) {
-        SqlParameterSource parameter = new MapSqlParameterSource().addValue(DBNames.ID, id);
+        SqlParameterSource parameter = new MapSqlParameterSource()
+                .addValue(DBNames.ID, id);
         try {
-            return this.namedParameterJdbcTemplate.queryForObject(uniqueById, parameter, rowMapper);
+            return this.namedParameterJdbcTemplate.queryForObject(uniqueById,
+                    parameter, rowMapper);
         } catch (DataAccessException e) {
             return null;
         }

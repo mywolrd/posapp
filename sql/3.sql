172ec08c3e4196e6699ce35b1b9840bc34dd6b40
mywolrd <moonbae77@gmail.com>
2016-11-22 15:20:31 -0800

    Table name change

--- sql/3.sql
+++ sql/3.sql
@@ -13,24 +13,34 @@ create table if not exists pos.orders (
 );
 
 create table if not exists pos.order_details (
-	id        serial    primary key,
-    orderId   integer   references pos.orders(id),
-    itemId    integer   references pos.items(id),
-    quantity  smallint,
-    newdollar smallint,
-    newcent   smallint
+	id        	serial    primary key,
+    orderId   	integer   references pos.orders(id),
+    itemId    	integer   references pos.items(id),
+    quantity  	smallint,
+    newdollar 	smallint,
+    newcent   	smallint,
+    active    	boolean,
+    createdAt  	timestamp,
+    updatedAt  	timestamp,
+    createdBy  	smallint,
+    updatedBy  	smallint
 );
 
-create table if not exists pos.order_details_addon_items (
+create table if not exists pos.order_detail_addon_items (
 	id				serial	primary key,
-	addOnItemId     integer references pos.addon_items(id),
+	addonItemId     integer references pos.addon_items(id),
 	orderDetailsId 	integer references pos.order_details(id),
 	newdollar  		smallint,
-	newcent    		smallint
+	newcent    		smallint,
+	active    		boolean,
+	createdAt  		timestamp,
+    updatedAt  		timestamp,
+    createdBy  		smallint,
+    updatedBy  		smallint
 );
 
-grant select, insert, delete, update on pos.order_details_addon_items to pos;
-grant select, usage on sequence pos.order_details_addon_items_id_seq to pos;
+grant select, insert, delete, update on pos.order_detail_addon_items to pos;
+grant select, usage on sequence pos.order_detail_addon_items_id_seq to pos;
 
 grant select, insert, delete, update on pos.order_details to pos;
 grant select, usage on sequence pos.order_details_id_seq to pos;
